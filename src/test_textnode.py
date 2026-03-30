db26064c453b249652e441792126580af3ae9b47
tct-willr <william.robertson@trout.com.au>
2026-03-31 09:30:50 +1100

    feat: split nodes fn

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,44 +1,66 @@
 import unittest
 
-from textnode import TextNode, TextType, text_node_to_html_node
+from textnode import TextNode, TextType, split_nodes_delimiter, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
-        node = TextNode("This is a text node", TextType.Bold)
-        node2 = TextNode("This is a text node", TextType.Bold)
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
 
     def test_unequal_string(self):
-        node = TextNode("This is a text node", TextType.Bold)
-        node2 = TextNode("This is another", TextType.Bold)
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is another", TextType.BOLD)
         self.assertNotEqual(node, node2)
 
     def test_unmatching_url(self):
-        node = TextNode("This is a text node", TextType.Bold, "https://example.com")
-        node2 = TextNode("This is a text node", TextType.Bold)
+        node = TextNode("This is a text node", TextType.BOLD, "https://example.com")
+        node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertNotEqual(node, node2)
 
     def test_unmatching_texttype(self):
-        node = TextNode("This is a text node", TextType.Bold)
-        node2 = TextNode("This is a text node", TextType.Italic)
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
         self.assertNotEqual(node, node2)
 
     def test_all_unmatching(self):
-        node = TextNode("This is a text node", TextType.Bold, "https://example.com")
-        node2 = TextNode("This is another", TextType.Italic, None)
+        node = TextNode("This is a text node", TextType.BOLD, "https://example.com")
+        node2 = TextNode("This is another", TextType.ITALIC, None)
         self.assertNotEqual(node, node2)
 
 
 class TestTextToHTMLNode(unittest.TestCase):
     def test_text(self):
-        node = TextNode("This is a text node", TextType.Text)
+        node = TextNode("This is a text node", TextType.TEXT)
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
 
     def test_bold(self):
-        node = TextNode("This is a bold node", TextType.Bold)
+        node = TextNode("This is a bold node", TextType.BOLD)
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "b")
         self.assertEqual(html_node.to_html(), "<b>This is a bold node</b>")
+
+
+class TestSplitNodes(unittest.TestCase):
+    def test_code_node(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        nodelist = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT),
+        ]
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertListEqual(nodelist, new_nodes)
+
+    def test_bold_node(self):
+        node = TextNode("This is text with a **bold block** word", TextType.TEXT)
+        nodelist = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("bold block", TextType.BOLD),
+            TextNode(" word", TextType.TEXT),
+        ]
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertListEqual(nodelist, new_nodes)
--- src/textnode.py
+++ src/textnode.py
@@ -4,16 +4,16 @@ from htmlnode import LeafNode
 
 
 class TextType(Enum):
-    Text = "text"
-    Bold = "bold"
-    Italic = "italic"
-    Code = "code"
-    Link = "link"
-    Image = "image"
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
 
 
 class TextNode:
-    def __init__(self, text, text_type: TextType, url=None):
+    def __init__(self, text: str, text_type: TextType, url: str | None = None):
         self.text = text
         self.text_type = text_type
         self.url = url
@@ -32,18 +32,40 @@ class TextNode:
 
 
 def text_node_to_html_node(text_node: TextNode):
+    """Turns a given TextNode to a LeafNode"""
     match text_node.text_type:
-        case TextType.Text:
+        case TextType.TEXT:
             return LeafNode(None, text_node.text)
-        case TextType.Bold:
+        case TextType.BOLD:
             return LeafNode("b", text_node.text)
-        case TextType.Italic:
+        case TextType.ITALIC:
             return LeafNode("i", text_node.text)
-        case TextType.Code:
+        case TextType.CODE:
             return LeafNode("code", text_node.text)
-        case TextType.Link:
+        case TextType.LINK:
             return LeafNode("a", text_node.text, {"href": text_node.url})
-        case TextType.Image:
+        case TextType.IMAGE:
             return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
         case _:
             raise Exception("somehow unsupported TextType")
+
+
+def split_nodes_delimiter(
+    old_nodes: list[TextNode], delimiter: str, text_type: TextType
+):
+    """Splits up a TEXT TextNode into new TextNodes based on a delimiter"""
+    new_nodes: list[TextNode] = []
+    for n in old_nodes:
+        if n.text_type != TextType.TEXT:
+            new_nodes.append(n)
+            continue
+        parts = n.text.split(delimiter)
+        if len(parts) % 2 == 0:
+            raise Exception("non-valid Markdown syntax")
+        for i, part in enumerate(parts):
+            if i % 2 != 0:
+                new_nodes.append(TextNode(part, text_type))
+            else:
+                new_nodes.append(TextNode(part, n.text_type))
+
+    return new_nodes
